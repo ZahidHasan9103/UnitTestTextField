1c60f01bd9bf2207548e53c9265e7669a41f627d
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-07-03 01:39:29 +0600

    test outlet connection ✅

--- UnitTestTextFieldTests/ViewControllerTests.swift
+++ UnitTestTextFieldTests/ViewControllerTests.swift
@@ -6,6 +6,18 @@
 //
 
 import XCTest
+@testable import UnitTestTextField
 
 final class ViewControllerTests: XCTestCase {
+    
+    func test_outlets_shouldBeConnected(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut:ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        
+        sut.loadView()
+        
+        XCTAssertNotNil(sut.userNameTextField, "usernameField")
+        XCTAssertNotNil(sut.passwordTextField, "passwordTextField")
+    }
+    
 }
